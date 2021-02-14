ed4a26b07a41f99df47064f54fca0399f93125fd
gitinun55 <54529430+gitinun55@users.noreply.github.com>
2021-02-15 01:29:21 +0700

    AppRunner15-02-64

---
+++ app/src/main/java/com/example/apprunner/Organizer/menu5_update_reward/Activity/InsertRewardActivity.java
@@ -0,0 +1,16 @@
+package com.example.apprunner.Organizer.menu5_update_reward.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apprunner.R;
+
+public class InsertRewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_reward);
+    }
+}
---
+++ app/src/main/java/com/example/apprunner/addressQuery.java
@@ -0,0 +1,95 @@
+package com.example.apprunner;
+
+public class addressQuery {
+
+    String Address;
+    String District;
+    String MueangDistrict;
+    String province;
+    String Country_number;
+    String name;
+    String tel;
+    String first_name;
+    String last_name;
+    int id_address;
+
+    public int getId_address(){
+        return id_address;
+    }
+
+    public void setId_address(){
+        this.id_address = id_address;
+    }
+
+    public String getAddress(){
+        return Address;
+    }
+
+    public void setAddress(){
+        this.Address = Address;
+    }
+
+    public String getDistrict(){
+        return District;
+    }
+
+    public void setDistrict(){
+        this.District = District;
+    }
+
+    public String getMueangDistrict(){
+        return MueangDistrict;
+    }
+
+    public void setMueangDistrict(){
+        this.MueangDistrict = MueangDistrict;
+    }
+
+    public String getProvince(){
+        return province;
+    }
+
+    public void setProvince(){
+        this.province = province;
+    }
+
+    public String getCountry_number(){
+        return Country_number;
+    }
+
+    public void setCountry_number(){
+        this.Country_number = Country_number;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public String getTel(){
+        return tel;
+    }
+
+    public void setTel(){
+        this.tel = tel;
+    }
+
+    public String getFirst_name(){
+        return first_name;
+    }
+
+    public void setFirst_name(){
+        this.first_name = first_name;
+    }
+
+    public String getLast_name(){
+        return last_name;
+    }
+
+    public void setLast_name(){
+        this.last_name = last_name;
+    }
+}
